339d23f7cbbdd76f2f88e48f2350350a93a81530
SuMitkuMar1303 <93091287+SuMitkuMar1303@users.noreply.github.com>
2022-06-30 22:45:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,564 @@
+function makeSound(key) {
+
+    switch (key) {
+        case "`":
+            var audio = new Audio('sound/A0.mp3');
+            audio.play();
+            break;
+        case "1":
+            var audio = new Audio('sound/B0.mp3');
+            audio.play();
+            break;
+        case "2":
+            var audio = new Audio('sound/C1.mp3');
+            audio.play();
+            break;
+        case "3":
+            var audio = new Audio('sound/D1.mp3');
+            audio.play();
+            break;
+        case "4":
+            var audio = new Audio('sound/E1.mp3');
+            audio.play();
+
+            break;
+        case "5":
+            var audio = new Audio('sound/F1.mp3');
+            audio.play();
+            break;
+        case "6":
+            var audio = new Audio('sound/G1.mp3');
+            audio.play();
+            break;
+        case "7":
+            var audio = new Audio('sound/A1.mp3');
+            audio.play();
+            break;
+        case "8":
+            var audio = new Audio('sound/B1.mp3');
+            audio.play();
+            break;
+        case "9":
+            var audio = new Audio('sound/C2.mp3');
+            audio.play();
+            break;
+        case "0":
+            var audio = new Audio('sound/D2.mp3');
+            audio.play();
+            break;
+        case "-":
+            var audio = new Audio('sound/E2.mp3');
+            audio.play();
+            break;
+        case "=":
+            var audio = new Audio('sound/F2.mp3');
+            audio.play();
+            break;
+        case "q":
+            var audio = new Audio('sound/G2.mp3');
+            audio.play();
+            break;
+        case "w":
+            var audio = new Audio('sound/A2.mp3');
+            audio.play();
+            break;
+        case "e":
+            var audio = new Audio('sound/B2.mp3');
+            audio.play();
+            break;
+        case "r":
+            var audio = new Audio('sound/C3.mp3');
+            audio.play();
+            break;
+        case "t":
+            var audio = new Audio('sound/D3.mp3');
+            audio.play();
+            break;
+        case "y":
+            var audio = new Audio('sound/E3.mp3');
+            audio.play();
+            break;
+        case "u":
+            var audio = new Audio('sound/F3.mp3');
+            audio.play();
+            break;
+        case "i":
+            var audio = new Audio('sound/G3.mp3');
+            audio.play();
+            break;
+        case "o":
+            var audio = new Audio('sound/A3.mp3');
+            audio.play();
+            break;
+        case "p":
+            var audio = new Audio('sound/B3.mp3');
+            audio.play();
+            break;
+        case "[":
+            var audio = new Audio('sound/C4.mp3');
+            audio.play();
+            break;
+        case "]":
+            var audio = new Audio('sound/D4.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sound/E4.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sound/F4.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sound/G4.mp3');
+            audio.play();
+            break;
+        case "f":
+            var audio = new Audio('sound/A4.mp3');
+            audio.play();
+            break;
+        case "g":
+            var audio = new Audio('sound/B4.mp3');
+            audio.play();
+            break;
+        case "h":
+            var audio = new Audio('sound/C5.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sound/D5.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sound/E5.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sound/F5.mp3');
+            audio.play();
+            break;
+        case ";":
+            var audio = new Audio('sound/G5.mp3');
+            audio.play();
+            break;
+        case "'":
+            var audio = new Audio('sound/A5.mp3');
+            audio.play();
+            break;
+        case "z":
+            var audio = new Audio('sound/B5.mp3');
+            audio.play();
+            break;
+        case "x":
+            var audio = new Audio('sound/C6.mp3');
+            audio.play();
+            break;
+        case "c":
+            var audio = new Audio('sound/D6.mp3');
+            audio.play();
+            break;
+        case "v":
+            var audio = new Audio('sound/E6.mp3');
+            audio.play();
+            break;
+        case "b":
+            var audio = new Audio('sound/F6.mp3');
+            audio.play();
+            break;
+        case "n":
+            var audio = new Audio('sound/G6.mp3');
+            audio.play();
+            break;
+        case "m":
+            var audio = new Audio('sound/A6.mp3');
+            audio.play();
+            break;
+        case ",":
+            var audio = new Audio('sound/B6.mp3');
+            audio.play();
+            break;
+        case ".":
+            var audio = new Audio('sound/C7.mp3');
+            audio.play();
+            break;
+        case "/":
+            var audio = new Audio('sound/D7.mp3');
+            audio.play();
+            break;
+
+        case "E":
+            var audio = new Audio('sound/E7.mp3');
+            audio.play();
+            break;
+        case "Y":
+            var audio = new Audio('sound/F7.mp3');
+            audio.play();
+            break;
+        case "P":
+            var audio = new Audio('sound/F7.mp3');
+            audio.play();
+            break;
+        case "A":
+            var audio = new Audio('sound/A7.mp3');
+            audio.play();
+            break;
+        case "G":
+            var audio = new Audio('sound/B7.mp3');
+            audio.play();
+            break;
+        case "K":
+            var audio = new Audio('sound/C8.mp3');
+            audio.play();
+            break;
+
+            // =============================================================
+        case "~":
+            var audio = new Audio('sound/Bb0.mp3');
+            audio.play();
+            break;
+        case "@":
+            var audio = new Audio('sound/Bb1.mp3');
+            audio.play();
+            break;
+        case "#":
+            var audio = new Audio('sound/Db1.mp3');
+            audio.play();
+            break;
+        case "%":
+            var audio = new Audio('sound/Eb1.mp3');
+            audio.play();
+            break;
+        case "^":
+            var audio = new Audio('sound/Gb1.mp3');
+            audio.play();
+
+            break;
+        case "&":
+            var audio = new Audio('sound/Ab1.mp3');
+            audio.play();
+            break;
+        case "(":
+            var audio = new Audio('sound/Bb2.mp3');
+            audio.play();
+            break;
+        case ")":
+            var audio = new Audio('sound/Db2.mp3');
+            audio.play();
+            break;
+        case "+":
+            var audio = new Audio('sound/Eb2.mp3');
+            audio.play();
+            break;
+        case "Q":
+            var audio = new Audio('sound/Gb2.mp3');
+            audio.play();
+            break;
+        case "W":
+            var audio = new Audio('sound/Ab2.mp3');
+            audio.play();
+            break;
+        case "R":
+            var audio = new Audio('sound/Bb3.mp3');
+            audio.play();
+            break;
+        case "T":
+            var audio = new Audio('sound/Db3.mp3');
+            audio.play();
+            break;
+        case "U":
+            var audio = new Audio('sound/Eb3.mp3');
+            audio.play();
+            break;
+        case "I":
+            var audio = new Audio('sound/Gb3.mp3');
+            audio.play();
+            break;
+        case "O":
+            var audio = new Audio('sound/Ab3.mp3');
+            audio.play();
+            break;
+        case "{":
+            var audio = new Audio('sound/Bb4.mp3');
+            audio.play();
+            break;
+        case "}":
+            var audio = new Audio('sound/Db4.mp3');
+            audio.play();
+            break;
+        case "S":
+            var audio = new Audio('sound/Eb4.mp3');
+            audio.play();
+            break;
+        case "D":
+            var audio = new Audio('sound/Gb4.mp3');
+            audio.play();
+            break;
+        case "F":
+            var audio = new Audio('sound/Ab4.mp3');
+            audio.play();
+            break;
+        case "H":
+            var audio = new Audio('sound/Bb5.mp3');
+            audio.play();
+            break;
+        case "J":
+            var audio = new Audio('sound/Db5.mp3');
+            audio.play();
+            break;
+        case "L":
+            var audio = new Audio('sound/Eb5.mp3');
+            audio.play();
+            break;
+        case ":":
+            var audio = new Audio('sound/Gb5.mp3');
+            audio.play();
+            break;
+            // case "":
+            //     var audio = new Audio('sound/E4.mp3');
+            //     audio.play();
+            //     break;
+        case "X":
+            var audio = new Audio('sound/Bb6.mp3');
+            audio.play();
+            break;
+        case "C":
+            var audio = new Audio('sound/Db6.mp3');
+            audio.play();
+            break;
+        case "B":
+            var audio = new Audio('sound/Eb6.mp3');
+            audio.play();
+            break;
+        case "N":
+            var audio = new Audio('sound/Gb6.mp3');
+            audio.play();
+            break;
+        case "M":
+            var audio = new Audio('sound/Ab6.mp3');
+            audio.play();
+            break;
+        case ">":
+            var audio = new Audio('sound/Bb7.mp3');
+            audio.play();
+            break;
+        case "?":
+            var audio = new Audio('sound/Db7.mp3');
+            audio.play();
+            break;
+        case "Z":
+            var audio = new Audio('sound/Eb7.mp3');
+            audio.play();
+            break;
+        case "V":
+            var audio = new Audio('sound/Gb7.mp3');
+            audio.play();
+            break;
+        case "<":
+            var audio = new Audio('sound/Ab7.mp3');
+            audio.play();
+            break;
+    }
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    var currentKey = event.key;
+    if (currentKey === "`") {
+
+        var activeButton = document.querySelector(".an0");
+        activeButton.classList.add("pressed");
+    } else if (currentKey >= "0" && currentKey <= "9") {
+        var activeButton = document.querySelector(".a" + currentKey);
+        activeButton.classList.add("pressed");
+    } else if (currentKey === "[") {
+        var activeButton = document.querySelector(".an1");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === "]") {
+        var activeButton = document.querySelector(".an2");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === ";") {
+        var activeButton = document.querySelector(".an3");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === "'") {
+        var activeButton = document.querySelector(".an4");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === ",") {
+        var activeButton = document.querySelector(".an5");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === ".") {
+        var activeButton = document.querySelector(".an6");
+        activeButton.classList.add("pressed");
+    } else if (currentKey === "/") {
+        var activeButton = document.querySelector(".an7");
+        activeButton.classList.add("pressed");
+    }
+    // ============================================
+    else if (currentKey === "~") {
+        var activeButton = document.querySelector(".A0");
+        activeButton.classList.add("press");
+    } else if (currentKey === "@") {
+        var activeButton = document.querySelector(".C1");
+        activeButton.classList.add("press");
+    } else if (currentKey === "#") {
+        var activeButton = document.querySelector(".D1");
+        activeButton.classList.add("press");
+    } else if (currentKey === "%") {
+        var activeButton = document.querySelector(".F1");
+        activeButton.classList.add("press");
+    } else if (currentKey === "^") {
+        var activeButton = document.querySelector(".G1");
+        activeButton.classList.add("press");
+    } else if (currentKey === "&") {
+        var activeButton = document.querySelector(".A1");
+        activeButton.classList.add("press");
+    } else if (currentKey === "(") {
+        var activeButton = document.querySelector(".C2");
+        activeButton.classList.add("press");
+    } else if (currentKey === ")") {
+        var activeButton = document.querySelector(".D2");
+        activeButton.classList.add("press");
+    } else if (currentKey === "+") {
+        var activeButton = document.querySelector(".F2");
+        activeButton.classList.add("press");
+    } else if (currentKey === "{") {
+        var activeButton = document.querySelector(".C4");
+        activeButton.classList.add("press");
+    } else if (currentKey === "}") {
+        var activeButton = document.querySelector(".D4");
+        activeButton.classList.add("press");
+    } else if (currentKey === ":") {
+        var activeButton = document.querySelector(".G5");
+        activeButton.classList.add("press");
+    } else if (currentKey === "<") {
+        var activeButton = document.querySelector(".A7");
+        activeButton.classList.add("press");
+    }
+    // else if (currentKey === "+") {
+    //     var activeButton = document.querySelector("F2");
+    //     activeButton.classList.remove("pressed");
+    // }
+    else if (currentKey === ">") {
+        var activeButton = document.querySelector(".C7");
+        activeButton.classList.add("press");
+    } else if (currentKey === "?") {
+        var activeButton = document.querySelector(".D7");
+        activeButton.classList.add("press");
+    } else {
+        if (currentKey >= "A" && currentKey <= "Z") {
+            var activeButton = document.querySelector("." + currentKey);
+            activeButton.classList.add("press");
+        } else {
+            var activeButton = document.querySelector("." + currentKey);
+            activeButton.classList.add("pressed");
+        }
+
+    }
+
+
+});
+document.addEventListener("keyup", function(event) {
+    var currentKey = event.key;
+    if (currentKey === "`") {
+        var activeButton = document.querySelector(".an0");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey >= "0" && currentKey <= "9") {
+        var activeButton = document.querySelector(".a" + currentKey);
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === "[") {
+        var activeButton = document.querySelector(".an1");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === "]") {
+        var activeButton = document.querySelector(".an2");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === ";") {
+        var activeButton = document.querySelector(".an3");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === "'") {
+        var activeButton = document.querySelector(".an4");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === ",") {
+        var activeButton = document.querySelector(".an5");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === ".") {
+        var activeButton = document.querySelector(".an6");
+        activeButton.classList.remove("pressed");
+    } else if (currentKey === "/") {
+        var activeButton = document.querySelector(".an7");
+        activeButton.classList.remove("pressed");
+    }
+    // ==========================================================================
+    else if (currentKey === "~") {
+        var activeButton = document.querySelector(".A0");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "@") {
+        var activeButton = document.querySelector(".C1");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "#") {
+        var activeButton = document.querySelector(".D1");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "%") {
+        var activeButton = document.querySelector(".F1");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "^") {
+        var activeButton = document.querySelector(".G1");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "&") {
+        var activeButton = document.querySelector(".A1");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "(") {
+        var activeButton = document.querySelector(".C2");
+        activeButton.classList.remove("press");
+    } else if (currentKey === ")") {
+        var activeButton = document.querySelector(".D2");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "+") {
+        var activeButton = document.querySelector(".F2");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "{") {
+        var activeButton = document.querySelector(".C4");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "}") {
+        var activeButton = document.querySelector(".D4");
+        activeButton.classList.remove("press");
+    } else if (currentKey === ":") {
+        var activeButton = document.querySelector(".G5");
+        activeButton.classList.remove("press");
+    }
+    // else if (currentKey === "+") {
+    //     var activeButton = document.querySelector("F2");
+    //     activeButton.classList.remove("pressed");
+    // }
+    else if (currentKey === ">") {
+        var activeButton = document.querySelector(".C7");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "?") {
+        var activeButton = document.querySelector(".D7");
+        activeButton.classList.remove("press");
+    } else if (currentKey === "<") {
+        var activeButton = document.querySelector(".A7");
+        activeButton.classList.remove("press");
+    } else {
+        if (currentKey >= "A" && currentKey <= "Z") {
+            var activeButton = document.querySelector("." + currentKey);
+            activeButton.classList.remove("press");
+        } else {
+            var activeButton = document.querySelector("." + currentKey);
+            activeButton.classList.remove("pressed");
+        }
+
+    }
+
+});
+
+
+
+
+// function buttonAnimation(currentKey) {
+//     var activeButton = document.querySelector("." + currentKey);
+//     activeButton.classList.add("pressed");
+//     setTimeout(function() {
+//         activeButton.classList.remove("pressed");
+//     }, 100);
+// }
